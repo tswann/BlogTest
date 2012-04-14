f3be99f92e822654dce7a89b04776c117fc08ac3
Thomas Swann <ts.swann@gmail.com>
2012-04-14 20:39:59 +0100

    Initial commit

---
+++ BlogTest/Models/Blog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BlogTest.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string BloggerName { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ BlogTest/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlogTest.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
