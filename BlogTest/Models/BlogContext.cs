9c78148b539428c4476cabca1c43f6a81345e0ae
Thomas Swann <ts.swann@gmail.com>
2012-04-14 23:44:44 +0100

    Added BlogContext class

---
+++ BlogTest/Models/BlogContext.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Data.Entity;
+
+namespace BlogTest.Models
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
